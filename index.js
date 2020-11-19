160a47a789b3e8dd35cf84f308ad094843e9a99a
ElOuastaniAziz <designsyaz@gmail.com>
2020-11-19 15:59:58 +0100

    port error resolved

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ app.delete('/delete',(req,res)=>{
 /*app.listen(3000,function(){
     console.log('Server on port 3000'.blue);
 })*/
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
